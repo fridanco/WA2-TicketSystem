30499e11516ffc867347222a99d0587d21dd94ef
fridanco <francesco.difranco97@gmail.com>
2022-03-18 19:00:32 +0100

    first things

--- server/src/main/kotlin/it/polito/wa2/g17/server/ValidationController.kt
+++ server/src/main/kotlin/it/polito/wa2/g17/server/ValidationController.kt
@@ -1,14 +1,20 @@
 package it.polito.wa2.g17.server
 
+import org.apache.el.util.Validation
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
+
 @RestController
 class ValidationController {
     @PostMapping("/validate")
-    fun helloWorld():String?{
-        return "sprint iniziamo male"
+    fun responseValidate(  @RequestBody body: ValidationObject): String? {
+        println(body);
+        return "$body.zone";
     }
 }
---
+++ server/src/main/kotlin/it/polito/wa2/g17/server/ValidationObject.kt
@@ -0,0 +1,6 @@
+package it.polito.wa2.g17.server
+
+class ValidationObject {
+     var zone: String = ""
+     var token: String = ""
+}
