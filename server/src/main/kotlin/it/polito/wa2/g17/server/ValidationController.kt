b117a02c40aa26146515256371d697adf58aed6d
mariodedapolito <s266280@studenti.polito.it>
2022-03-19 02:51:24 +0100

    complete jwt validation and payload decoding

--- server/src/main/kotlin/it/polito/wa2/g17/server/ValidationController.kt
+++ server/src/main/kotlin/it/polito/wa2/g17/server/ValidationController.kt
@@ -1,39 +1,49 @@
 package it.polito.wa2.g17.server
 
-import io.jsonwebtoken.io.Decoders
-import io.jsonwebtoken.JwtException
-import io.jsonwebtoken.Jwts
-import io.jsonwebtoken.SignatureAlgorithm
-import io.jsonwebtoken.SigningKeyResolver
-import io.jsonwebtoken.security.Keys
+import io.jsonwebtoken.*
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import java.security.Key
+import java.util.*
+import javax.crypto.spec.SecretKeySpec
 
 
-val key: Key = Keys.secretKeyFor(SignatureAlgorithm.HS256)
+//Encode JWT with this secret and select Base64 encoded
+var secret = "asdfSFS34wfsdfsdfSDSD32dfsddDDerQSNCK34SOWEK5354fdgdf4"
+var hmacKey: Key = SecretKeySpec(Base64.getDecoder().decode(secret),SignatureAlgorithm.HS256.jcaName)
 
 
 @RestController
 class ValidationController {
     @PostMapping("/validate")
     fun responseValidate(  @RequestBody validationObject: ValidationObject) : ResponseEntity<Any> {
-        //Print the received object
-        println(validationObject.zone)
-        println(validationObject.token)
 
-        //JWT operations
+        var validatedJwt : Jws<Claims>
+
+        //JWT validation
         try {
-
-            //OK, we can trust this JWT
+            validatedJwt = Jwts
+                .parserBuilder()
+                .setSigningKey(hmacKey)
+                .build()
+                .parseClaimsJws(validationObject.token)
+            println("--valid token--")
         } catch (e: JwtException) {
-            println("invalid token")
+            println("invalid token: ${e.message}")
             return ResponseEntity("",HttpStatus.FORBIDDEN)
         }
 
+        //Get JWT body (payload) claims
+        val expiration = validatedJwt.body["exp"]
+        val validityZone = validatedJwt.body["vz"]
+        println("expiration: $expiration")
+        println("validity zones: $validityZone")
+
+        //insert checks
+        //expiration time check already done by jjwt at validation
 
         //Return response with empty body & code 200
         return ResponseEntity("",HttpStatus.OK)
