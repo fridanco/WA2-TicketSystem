e87b90bde2330de4e3f79c43e27c1dfca3f28977
mariodedapolito <mario.deda@studenti.polito.it>
2022-03-26 01:03:24 +0100

    done create csv for expected throughput

--- server/src/main/kotlin/it/polito/wa2/g17/csv_creator/CsvCreator.kt
+++ server/src/main/kotlin/it/polito/wa2/g17/csv_creator/CsvCreator.kt
@@ -3,23 +3,26 @@ package it.polito.wa2.g17.csv_creator
 import com.codahale.usl4j.Measurement
 import com.codahale.usl4j.Model
 import org.json.JSONObject
+import java.io.BufferedWriter
+import java.io.FileWriter
 import java.nio.file.Files
 import java.nio.file.Path
-import java.util.*
 
 
 fun main() {
-    val csvCreator : Example = Example()
+    val csvCreator = CsvCreator()
     csvCreator.buildModel()
 }
 
-internal class Example {
+internal class CsvCreator {
     fun buildModel() {
 
         val testData = mutableListOf<DoubleArray>()
+        val expectedThroughputs = mutableListOf<DoubleArray>()
 
         for(concurrencyLevel in arrayOf(1,2,4,8,16,32)){
-            val fileName: Path = Path.of("../../../../benchmark/LoadTestResults/result_$concurrencyLevel")
+            val fileName: Path = Path.of("./benchmark/LoadTestResults/result_$concurrencyLevel")
+
             val json = Files.readString(fileName)
 
             val obj = JSONObject(json)
@@ -27,30 +30,26 @@ internal class Example {
             testData.add(doubleArrayOf(concurrencyLevel.toDouble(), obj.getDouble("rps")))
         }
 
-
-
-
-
-
-        val points = arrayOf(
-            doubleArrayOf(1.0, 955.16),
-            doubleArrayOf(2.0, 1878.91),
-            doubleArrayOf(3.0, 2688.01),
-            doubleArrayOf(1.0, 955.16),
-            doubleArrayOf(2.0, 1878.91),
-            doubleArrayOf(3.0, 2688.01)
-        ) // etc.
-
         // Map the points to measurements of concurrency and throughput, then build a model from them.
-        val model = Arrays.stream(testData)
+        val model = testData
+            .stream()
             .map { point: DoubleArray? ->
                 Measurement.ofConcurrency().andThroughput(point)
             }
             .collect(Model.toModel())
-        var i = 10
-        while (i < 200) {
-            System.out.printf("At %d workers, expect %f req/sec\n", i, model.throughputAtConcurrency(i.toDouble()))
-            i += 10
+        var i = 1
+        while (i <= 200) {
+            expectedThroughputs.add(doubleArrayOf(i.toDouble(),model.throughputAtConcurrency(i.toDouble())))
+            i += 1
         }
+
+        val writer = BufferedWriter(FileWriter("./server/src/main/kotlin/it/polito/wa2/g17/csv_creator/expected_throughput.csv"))
+
+        expectedThroughputs.forEach{
+            writer.write(""+it[0].toInt()+","+it[1]+"\n")
+        }
+
+        writer.close()
+
     }
 }
